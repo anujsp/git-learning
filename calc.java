4eb5b06f233247e37c0c960b9c7ba05e73ff4b2a
Anuj Phophalia <anuj.phophalia@gmail.com>
2022-05-04 19:41:41 -0700

    Added calc.java file

---
+++ calc.java
@@ -0,0 +1,26 @@
+public class calc {
+
+    public static void main(String[] args) {
+
+        add(7,20);
+        subtract(20,7);
+        multiply(7,20);
+        divide(20,7);
+    }
+
+    private static void add(int a, int b) {
+        System.out.println("Nothing is calculated in add method..!!");
+    }
+
+    private static void subtract(int a, int b) {
+        System.out.println("Nothing is calculated in subtract method..!!");
+    }
+
+    private static void multiply(int a, int b) {
+        System.out.println("Nothing is calculated in multiply method..!!");
+    }
+
+    private static void divide(int a, int b) {
+        System.out.println("Nothing is calculated in divide method..!!");
+    }
+}
